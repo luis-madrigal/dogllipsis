bf77d9d11f0915e5cebbd290f4648639e952d3af
luis-madrigal <lmadrigal.111@gmail.com>
2016-03-14 13:46:32 +0800

    Update Task.java

--- [02] DesignChallenge/src/com/entry/Task.java
+++ [02] DesignChallenge/src/com/entry/Task.java
@@ -1,5 +1,12 @@
 package com.entry;
 
-public class Task {
+public class Task extends Entry{
+  
+  public boolean isSelected;
+	
+	public Task(String description, Date dateDue, Time startTime, Time endTime) {
+		
+	}
+
 
 }
