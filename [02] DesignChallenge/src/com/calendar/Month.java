66b107f57c72cc3e127419a9a9ed9336226ee901
luis-madrigal <lmadrigal.111@gmail.com>
2016-03-14 13:51:45 +0800

    Update Month.java

--- [02] DesignChallenge/src/com/calendar/Month.java
+++ [02] DesignChallenge/src/com/calendar/Month.java
@@ -8,6 +8,31 @@ public class Month {
 	private int month;
 	private int startOfMonth;
 	private int endOfMonth;
+	
+	public ArrayList<Day> getDays() {
+		return days;
+	}
+	public void setDays(ArrayList<Day> days) {
+		this.days = days;
+	}
+	public int getMonth() {
+		return month;
+	}
+	public void setMonth(int month) {
+		this.month = month;
+	}
+	public int getStartOfMonth() {
+		return startOfMonth;
+	}
+	public void setStartOfMonth(int startOfMonth) {
+		this.startOfMonth = startOfMonth;
+	}
+	public int getEndOfMonth() {
+		return endOfMonth;
+	}
+	public void setEndOfMonth(int endOfMonth) {
+		this.endOfMonth = endOfMonth;
+	}
 
 
 }
