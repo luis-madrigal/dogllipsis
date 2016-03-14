cd1abea53101162adfd1493c58e7646e319a0e98
luis-madrigal <lmadrigal.111@gmail.com>
2016-03-14 13:45:02 +0800

    Create EntryFactory.java

---
+++ [02] DesignChallenge/src/com/entry/EntryFactory.java
@@ -0,0 +1,9 @@
+package com.entry;
+
+public abstract class EntryFactory {
+	
+	public static Entry createEntry(EntryType type, String description, Date dateDue, Time startTime, Time endTime) {
+		
+	}
+
+}
