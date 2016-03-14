d9221d0734cc001c2dd8e0e30aa3ac9810ef95cd
Maynard Si <jan.si1997@gmail.com>
2016-03-14 19:32:37 +0800

    Update ProductivityToolController.java

--- [02] DesignChallenge/src/com/controller/ProductivityToolController.java
+++ [02] DesignChallenge/src/com/controller/ProductivityToolController.java
@@ -3,5 +3,11 @@ package com.view;
 import com.controller.Controller;
 
 public abstract class ProductivityToolController extends Controller{
-
+  
+  @Override
+	public void getEntries(Date date) {
+		// TODO Auto-generated method stub
+		
+	}
+	
 }
