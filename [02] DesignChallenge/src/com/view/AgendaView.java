4c337da0926c8332a5b3497aacc2cddffeab3f56
MZRaymundo <mz.raym@gmail.com>
2016-03-14 14:00:42 +0800

    Create AgendaView.java

---
+++ [02] DesignChallenge/src/com/view/AgendaView.java
@@ -0,0 +1,29 @@
+package com.view;
+
+public class AgendaView extends EntryView{
+
+	@Override
+	public void viewTasks() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void viewEvents() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void viewAll() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void viewNone() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
