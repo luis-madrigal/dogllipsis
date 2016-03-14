9f7e7da02aeb9f36357c4fe3ce00cf674f71b328
Maynard Si <jan.si1997@gmail.com>
2016-03-14 20:00:20 +0800

    Update ProductivityToolController.java

--- [02] DesignChallenge/src/com/controller/ProductivityToolController.java
+++ [02] DesignChallenge/src/com/controller/ProductivityToolController.java
@@ -1,13 +1,45 @@
-package com.view;
+package com.controller;
 
-import com.controller.Controller;
+import java.sql.Time;
+import java.util.ArrayList;
+import java.util.Date;
 
-public abstract class ProductivityToolController extends Controller{
-  
-  @Override
-	public void getEntries(Date date) {
+import com.calendar.TimeRange;
+import com.entry.Entry;
+import com.entry.EntryType;
+
+public class ProductivityToolController extends Controller{
+
+
+	@Override
+	public ArrayList<Entry> getEntries(java.sql.Date date) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public ArrayList<Entry> getEntries(java.sql.Date date, EntryType type) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void addEntries(EntryType type, String description, java.sql.Date dateDue, Time startTime, Time endTime) {
 		// TODO Auto-generated method stub
 		
 	}
-	
+
+	@Override
+	public boolean isValidEntry(java.sql.Date date, TimeRange timeRange) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void removeEntries(Entry entry) {
+		// TODO Auto-generated method stub
+		
+	}
+
 }
+
