8c07d50e9d2008aeb567d1a97aac952239c95a9a
luis-madrigal <lmadrigal.111@gmail.com>
2016-03-14 13:54:25 +0800

    Update Entry.java

--- [02] DesignChallenge/src/com/entry/Entry.java
+++ [02] DesignChallenge/src/com/entry/Entry.java
@@ -5,9 +5,41 @@ import java.awt.Color;
 import com.calendar.TimeRange;
 
 public abstract class Entry {
-	private TimeRange timeRange;
-	private String description;
-	private Color color;
-	private boolean isCompleted;
-	private Date dateDue;
+	protected TimeRange timeRange;
+	protected String description;
+	protected Color color;
+	protected boolean isCompleted;
+	protected Date dateDue;
+	
+	public TimeRange getTimeRange() {
+		return timeRange;
+	}
+	public void setTimeRange(TimeRange timeRange) {
+		this.timeRange = timeRange;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public Color getColor() {
+		return color;
+	}
+	public void setColor(Color color) {
+		this.color = color;
+	}
+	public boolean isCompleted() {
+		return isCompleted;
+	}
+	public void setCompleted(boolean isCompleted) {
+		this.isCompleted = isCompleted;
+	}
+	public Date getDateDue() {
+		return dateDue;
+	}
+	public void setDateDue(Date dateDue) {
+		this.dateDue = dateDue;
+	}
+
 }
