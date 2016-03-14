adaaea554e410f5e3b7f28fb92fe051fd263004c
MZRaymundo <mz.raym@gmail.com>
2016-03-14 13:56:15 +0800

    Update Model.java

--- [02] DesignChallenge/src/com/model/Model.java
+++ [02] DesignChallenge/src/com/model/Model.java
@@ -1,14 +1,23 @@
 package com.model;
 
+import java.sql.Date;
+import java.sql.Time;
 import java.util.ArrayList;
-import java.util.Calendar;
-import java.util.Date;
 
-import com.entry.Entry;
+import javax.swing.JFrame;
 
 public abstract class Model {
-	
-	private Calendar calendar;
-	public abstract ArrayList<Entry> getEntries(Date date);
 
+	protected Calendar calendar;
+	protected Handler handler;
+	
+	public abstract ArrayList<Entry> getEntries(Date date);
+	public abstract ArrayList<Entry> getEntries(Date date, EntryType type);
+	
+	public abstract void addEntries(EntryType type, String description, Date dateDue, Time startTime, Time endTime);
+	public abstract boolean isValidEntry(Date date, TimeRange timeRange);
+	
+	public abstract void ExportFile(String fileName, ArrayList<Entry> entries);
+	public abstract void removeEntries(Entry entry);
+	public abstract void importFile(JFrame baseFrame);
 }
