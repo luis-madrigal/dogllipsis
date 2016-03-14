2cfa9c931ef3cfe4adbd02d9da924c62de65c390
luis-madrigal <lmadrigal.111@gmail.com>
2016-03-14 13:52:29 +0800

    Update Year.java

--- [02] DesignChallenge/src/com/calendar/Year.java
+++ [02] DesignChallenge/src/com/calendar/Year.java
@@ -6,4 +6,17 @@ public class Year {
 	
 	private ArrayList<Month> months;
 	private int year;
+	
+	public ArrayList<Month> getMonths() {
+		return months;
+	}
+	public void setMonths(ArrayList<Month> months) {
+		this.months = months;
+	}
+	public int getYear() {
+		return year;
+	}
+	public void setYear(int year) {
+		this.year = year;
+	}
 }
