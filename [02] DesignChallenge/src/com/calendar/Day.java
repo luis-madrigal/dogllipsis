5d1f4a9241a20a08363cd84b5be76059f126bef3
luis-madrigal <lmadrigal.111@gmail.com>
2016-03-15 23:03:57 +0800

    Update Day.java

--- [02] DesignChallenge/src/com/calendar/Day.java
+++ [02] DesignChallenge/src/com/calendar/Day.java
@@ -10,16 +10,24 @@ public class Day {
 	private ArrayList<Entry> entries;
 	private boolean[] timeslots;
 	
+	public Day(int day) {
+		this.day = day;
+		this.entries = new ArrayList<Entry>();
+		for(int i = 0; i < 48; i++) {
+			this.entries.add(null);
+		}
+	}
+	
 	public int getDay() {
 		return day;
 	}
 	public void setDay(int day) {
 		this.day = day;
 	}
-	public Entry[] getEntries() {
+	public ArrayList<Entry> getEntries() {
 		return entries;
 	}
-	public void setEntries(Entry[] entries) {
+	public void setEntries(ArrayList<Entry> entries) {
 		this.entries = entries;
 	}
 	public boolean[] getTimeslots() {
