6b0bd93f94522d86127413d49d67eadcd375aea0
luis-madrigal <lmadrigal.111@gmail.com>
2016-03-15 20:13:41 +0800

    Update EntryFactory.java

--- [02] DesignChallenge/src/com/entry/EntryFactory.java
+++ [02] DesignChallenge/src/com/entry/EntryFactory.java
@@ -3,6 +3,14 @@ package com.entry;
 public abstract class EntryFactory {
 	
 	public static Entry createEntry(EntryType type, String description, Date dateDue, Time startTime, Time endTime) {
+		Entry newEntry = null;
+		if(type == EntryType.EVENT) {
+			newEntry = new Event(description, dateDue, startTime, endTime);
+		}
+		else if(type == EntryType.TASK) {
+			newEntry = new Task(description, dateDue, startTime, endTime);
+		}
+		return newEntry;
 		
 	}
 
