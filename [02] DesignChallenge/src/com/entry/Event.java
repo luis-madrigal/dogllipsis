1a6e919984d1bc36d7345d4859371a8c7114ea6d
luis-madrigal <lmadrigal.111@gmail.com>
2016-03-15 20:15:07 +0800

    Update Event.java

--- [02] DesignChallenge/src/com/entry/Event.java
+++ [02] DesignChallenge/src/com/entry/Event.java
@@ -2,8 +2,13 @@ package com.entry;
 
 public class Event extends Entry{
   
-  public Event(String description, Date dateDue, Time startTime, Time endTime) {
-		
+  	public Event(String description, Date dateDue, Time startTime, Time endTime) {
+		this.description = description;
+		this.dateDue = dateDue;
+		this.timeRange.setStartTime(startTime);
+		this.timeRange.setEndTime(endTime);
+		this.color = Color.BLUE;
+		this.isCompleted = false;
 	}
 
 }
