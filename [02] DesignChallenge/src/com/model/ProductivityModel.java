bc338dc8ef1a2dd0886979764568d5abd00e928b
Maynard Si <jan.si1997@gmail.com>
2016-03-15 20:48:42 +0800

    Create ProductivityModel.java

---
+++ [02] DesignChallenge/src/com/model/ProductivityModel.java
@@ -0,0 +1,114 @@
+package com.model;
+
+import java.util.Date;
+import java.util.GregorianCalendar;
+import java.sql.Time;
+import java.util.ArrayList;
+
+import javax.swing.JFrame;
+
+import com.calendar.Calendar;
+import com.calendar.TimeRange;
+import com.entry.Entry;
+import com.entry.EntryFactory;
+import com.entry.Event;
+import com.entry.Task;
+import com.view.EntryType;
+
+public class ProductivityToolModel extends Model {
+
+	public ProductivityToolModel() {
+		this.calendar = new Calendar();
+		
+	}
+	
+	@Override
+	public ArrayList<Entry> getEntries(Date date) {
+		String strYear = date.toString().substring(date.toString().length()-4, date.toString().length());
+		int year = Integer.parseInt(strYear);		
+		GregorianCalendar calendar = new GregorianCalendar(year, date.getMonth(), date.getDate());		
+		int month = calendar.get(calendar.MONTH);
+		int day = calendar.get(calendar.DATE);	
+		
+		int minYear = java.util.Calendar.getInstance().get(java.util.Calendar.YEAR)-100;
+		int indexYear = year-minYear;		
+		int indexMonth = month;
+		int indexDay = day-1;
+		
+		System.out.println("year: "+year);
+		System.out.println("month: "+month);
+		System.out.println("day: "+day);
+		System.out.println("indexYear: "+indexYear);
+		return this.calendar.getYears().get(indexYear).getMonths().get(indexMonth).getDays().get(indexDay).getEntries();
+	}
+
+	@Override
+	public ArrayList<Entry> getEntries(Date date, EntryType type) {
+		ArrayList<Entry> entries = this.getEntries(date);
+		ArrayList<Entry> sortedEntries = new ArrayList<Entry>();
+		
+		switch(type) {
+			case EVENT:
+				for(int i = 0; i < entries.size(); i++) {
+					if(entries.get(i) instanceof Event) {
+						sortedEntries.add(entries.get(i));
+					}					
+				}				
+				break;
+				
+			case TASK:
+				for(int i = 0; i < entries.size(); i++) {
+					if(entries.get(i) instanceof Task) {
+						sortedEntries.add(entries.get(i));
+					}					
+				}				
+				break;
+				
+			case NONE:
+				sortedEntries = entries;
+				break;
+		}
+		
+		return null;
+	}
+
+	@Override
+	public void addEntries(EntryType type, String description, Date dateDue,
+			Time startTime, Time endTime) {
+		// TODO Auto-generated method stub
+		switch(type){
+			case EVENT:
+				this.getEntries(dateDue, type).add(EntryFactory.createEntry(type, description, dateDue, startTime, endTime));
+				break;
+			case TASK:
+				this.getEntries(dateDue, type).add(EntryFactory.createEntry(type, description, dateDue, startTime, endTime));
+				break;
+		}	
+	}
+
+	@Override
+	public boolean isValidEntry(Date date, TimeRange timeRange) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void ExportFile(String fileName, ArrayList<Entry> entries) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void removeEntries(Entry entry) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void importFile(JFrame baseFrame) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	
+}
