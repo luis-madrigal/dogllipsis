83cf275d2f4a06a8ce157215f6ce26325595c1eb
luis-madrigal <lmadrigal.111@gmail.com>
2016-03-15 22:41:25 +0800

    Create DataFileFilter.java

---
+++ [02] DesignChallenge/src/com/gui/DataFileFilter.java
@@ -0,0 +1,40 @@
+package com.model;
+
+import java.io.File;
+
+import javax.swing.filechooser.FileFilter;
+ 
+public class DataFileFilter extends FileFilter {
+ 
+    public boolean accept(File file) {
+        if (file.isDirectory()) {
+            return true;
+        }
+ 
+        String extension = DataFileFilter.getExtension(file.getName());
+        if (extension != null) {
+            if (extension.equals("csv")) {
+                    return true;
+            } else {
+                return false;
+            }
+        }
+ 
+        return false;
+    }
+    
+    public static String getExtension(String f) {
+    	String ext = null;
+        int i = f.lastIndexOf('.');
+
+        if (i > 0 &&  i < f.length() - 1) {
+            ext = f.substring(i+1).toLowerCase();
+        }        
+        return ext;
+    }
+
+	@Override
+	public String getDescription() {
+		return "Just CSV files";
+	}
+}
