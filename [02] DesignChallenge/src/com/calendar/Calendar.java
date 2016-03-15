cb4dad24ce76f9de7b69c89039c7224215689931
luis-madrigal <lmadrigal.111@gmail.com>
2016-03-15 23:03:24 +0800

    Update Calendar.java

--- [02] DesignChallenge/src/com/calendar/Calendar.java
+++ [02] DesignChallenge/src/com/calendar/Calendar.java
@@ -1,10 +1,7 @@
 package com.calendar;
 
-import java.io.FileWriter;
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.GregorianCalendar;
-
 
 
 public class Calendar {
@@ -19,54 +16,22 @@ public class Calendar {
 	private String[] months;
 	public Calendar(){
 
-		this.years = new ArrayList<CalYear>();		
-		this.minYear = Calendar.getInstance().get(Calendar.YEAR)-100;
+		this.years = new ArrayList<Year>();		
+		this.minYear = java.util.Calendar.getInstance().get(java.util.Calendar.YEAR)-100;
 		
 		for(int i = 0; i < 200; i++){
-			this.years.add(new CalYear(this.minYear+i));
+			this.years.add(new Year(this.minYear+i));
 		}
 		GregorianCalendar cal = new GregorianCalendar();
 		this.dayBound = cal.get(GregorianCalendar.DAY_OF_MONTH);
 		this.monthToday = cal.get(GregorianCalendar.MONTH);
 		this.yearToday = cal.get(GregorianCalendar.YEAR);
 		this.yearBound = 100;
-		this.row = 0;
-		this.column = 0;
+		
 		String[] months =  {"January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"};
 		this.months = months;
-		this.handler = new Handler();
-		this.tableRenderer = new TableRenderer(); //**************************************
-		
-		for (int i = this.yearToday-yearBound; i <= this.yearToday+yearBound; i++){
-			cmbYear.addItem(String.valueOf(i));
-		}			
-		this.yearToday = cal.get(GregorianCalendar.YEAR);
-		
-		//TODO New A
-		//Change this to the directory of the CSV file you want to preload.
-		String defaultPath = System.getProperty("user.home") + System.getProperty("file.separator")+"Calendar.csv";
-		try {			
-			handler.openFile(defaultPath);
-		} catch (IOException e1) {
-			e1.printStackTrace();
-			FileWriter writer = new FileWriter(defaultPath);
-			writer.flush();
-			writer.close();
-		}
-		ArrayList<CalEvent> importedEvents = handler.getEvents();		
-		for(int i = 0; i < importedEvents.size(); i++) {
-			this.addEvent(importedEvents.get(i));
-		}
-		//TODO New Z
-		refreshCalendar(this.monthToday, this.yearToday);
-	}
-	
-	public Year[] getYear() {
-		return year;
-	}
 
-	public void setYear(Year[] year) {
-		this.year = year;
+//		String defaultPath = System.getProperty("user.home") + System.getProperty("file.separator")+"Calendar.csv";
 	}
 
 	public int getYearBound() {
@@ -115,6 +80,22 @@ public class Calendar {
 
 	public void setMonths(String[] months) {
 		this.months = months;
+	}
+
+	public ArrayList<Year> getYears() {
+		return years;
+	}
+
+	public void setYears(ArrayList<Year> years) {
+		this.years = years;
+	}
+
+	public int getYearToday() {
+		return yearToday;
+	}
+
+	public void setYearToday(int yearToday) {
+		this.yearToday = yearToday;
 	}
 
 }
