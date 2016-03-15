035d861616dcdb819396db2d22251361c0a95cce
luis-madrigal <lmadrigal.111@gmail.com>
2016-03-15 21:55:17 +0800

    Create ExportCsv.java

---
+++ [02] DesignChallenge/src/com/model/ExportCsv.java
@@ -0,0 +1,39 @@
+package com.model;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+
+import com.entry.Entry;
+
+public class ExportCsv extends ExportFile
+{
+	public void generate(String sFileName, ArrayList<Entry> entries) {
+		try
+		{
+			FileWriter writer = new FileWriter(sFileName+".csv");
+			
+			for(int i = 0; i < entries.size(); i++) {
+				writer.append(String.valueOf(entries.get(i).getTimeRange().getStartTime().getTime()));
+				writer.append(",");
+				writer.append(String.valueOf(entries.get(i).getTimeRange().getEndTime().getTime()));
+				writer.append(",");
+				writer.append(entries.get(i).getDescription());
+				writer.append(",");
+				writer.append(String.valueOf(entries.get(i).getColor().getRGB()));
+				writer.append(",");
+				writer.append(String.valueOf(entries.get(i).isCompleted()));
+				writer.append(",");
+				writer.append(String.valueOf(entries.get(i).getDateDue().getTime()));
+				writer.append(System.getProperty("line.separator"));
+			}
+			
+		    writer.flush();
+		    writer.close();
+		}
+		catch(IOException e)
+		{
+		     e.printStackTrace();
+		} 
+  }
+}
