2fb6b1be734efcae80ffae0f73b5880998450f28
luis-madrigal <lmadrigal.111@gmail.com>
2016-03-15 23:05:52 +0800

    Update TimeRange.java

--- [02] DesignChallenge/src/com/calendar/TimeRange.java
+++ [02] DesignChallenge/src/com/calendar/TimeRange.java
@@ -6,6 +6,10 @@ public class TimeRange {
 	private Time startTime;
 	private Time endTime;
 	
+	public TimeRange(Time startTime, Time endTime) {
+		this.startTime = startTime;
+		this.endTime = endTime;
+	}
 	public Time getStartTime() {
 		return startTime;
 	}
