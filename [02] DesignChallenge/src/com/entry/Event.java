ab8fb7b957483c1ff944e7f9741d710b1b6483bd
MZRaymundo <mz.raym@gmail.com>
2016-03-16 20:17:02 +0900

    Update Event.java

--- [02] DesignChallenge/src/com/entry/Event.java
+++ [02] DesignChallenge/src/com/entry/Event.java
@@ -1,12 +1,17 @@
 package com.entry;
 
+import java.awt.Color;
+import java.sql.Time;
+import java.util.Date;
+
+import com.calendar.TimeRange;
+
 public class Event extends Entry{
   
   	public Event(String description, Date dateDue, Time startTime, Time endTime) {
 		this.description = description;
 		this.dateDue = dateDue;
-		this.timeRange.setStartTime(startTime);
-		this.timeRange.setEndTime(endTime);
+		this.timeRange = new TimeRange(startTime, endTime);
 		this.color = Color.BLUE;
 		this.isCompleted = false;
 	}
