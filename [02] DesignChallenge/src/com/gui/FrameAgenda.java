4892190aa2f081fda478fa78880f95c02bc57714
MZRaymundo <mz.raym@gmail.com>
2016-03-16 20:22:20 +0900

    Update FrameAgenda.java

--- [02] DesignChallenge/src/com/gui/FrameAgenda.java
+++ [02] DesignChallenge/src/com/gui/FrameAgenda.java
@@ -2,7 +2,6 @@ package com.gui;
 
 import java.awt.Color;
 import java.awt.Dimension;
-import java.awt.FlowLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.MouseEvent;
