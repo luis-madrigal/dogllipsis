9a28164d590dca2a3daeb1fb77fb7b2727ac8d5e
MZRaymundo <mz.raym@gmail.com>
2016-03-16 20:28:05 +0900

    Update ProductivityModel.java

--- [02] DesignChallenge/src/com/model/ProductivityModel.java
+++ [02] DesignChallenge/src/com/model/ProductivityModel.java
@@ -1,9 +1,11 @@
 package com.model;
 
-import java.util.Date;
-import java.util.GregorianCalendar;
+import java.io.File;
+import java.io.IOException;
 import java.sql.Time;
 import java.util.ArrayList;
+import java.util.Date;
+import java.util.GregorianCalendar;
 
 import javax.swing.JFrame;
 
@@ -13,32 +15,31 @@ import com.entry.Entry;
 import com.entry.EntryFactory;
 import com.entry.Event;
 import com.entry.Task;
+import com.gui.Application;
 import com.view.EntryType;
 
 public class ProductivityToolModel extends Model {
 
 	public ProductivityToolModel() {
 		this.calendar = new Calendar();
-		
+		this.handler = new Handler();
 	}
 	
+	@SuppressWarnings("deprecation")
 	@Override
 	public ArrayList<Entry> getEntries(Date date) {
+		System.out.println("date: "+date);
 		String strYear = date.toString().substring(date.toString().length()-4, date.toString().length());
 		int year = Integer.parseInt(strYear);		
 		GregorianCalendar calendar = new GregorianCalendar(year, date.getMonth(), date.getDate());		
-		int month = calendar.get(calendar.MONTH);
-		int day = calendar.get(calendar.DATE);	
+		int month = calendar.get(java.util.Calendar.MONTH);
+		int day = calendar.get(java.util.Calendar.DATE);	
 		
 		int minYear = java.util.Calendar.getInstance().get(java.util.Calendar.YEAR)-100;
 		int indexYear = year-minYear;		
 		int indexMonth = month;
 		int indexDay = day-1;
-		
-		System.out.println("year: "+year);
-		System.out.println("month: "+month);
-		System.out.println("day: "+day);
-		System.out.println("indexYear: "+indexYear);
+		System.out.println("indexYear: "+minYear);
 		return this.calendar.getYears().get(indexYear).getMonths().get(indexMonth).getDays().get(indexDay).getEntries();
 	}
 
@@ -49,51 +50,84 @@ public class ProductivityToolModel extends Model {
 		
 		switch(type) {
 			case EVENT:
-				for(int i = 0; i < entries.size(); i++) {
-					if(entries.get(i) instanceof Event) {
+				for(int i = 0; i < entries.size(); i++) {					
+					if(entries.get(i) instanceof Task) {
+						sortedEntries.add(null);
+					}				
+					else {
 						sortedEntries.add(entries.get(i));
-					}					
+					}
 				}				
 				break;
 				
 			case TASK:
 				for(int i = 0; i < entries.size(); i++) {
-					if(entries.get(i) instanceof Task) {
-						sortedEntries.add(entries.get(i));
+					if(entries.get(i) instanceof Event) {
+						sortedEntries.add(null);
 					}					
+					else {
+						sortedEntries.add(entries.get(i));
+					}
 				}				
 				break;
 				
 			case NONE:
-				sortedEntries = entries;
+				for(int i = 0; i < entries.size(); i++) {
+					sortedEntries.add(null);					
+				}		
 				break;
 		}
 		
-		return null;
+		return sortedEntries;
 	}
 
+	@SuppressWarnings("deprecation")
 	@Override
 	public void addEntries(EntryType type, String description, Date dateDue,
 			Time startTime, Time endTime) {
-		// TODO Auto-generated method stub
-		if(isValidEntry(dateDue, new TimeRange(startTime, endTime)))
-			this.getEntries(dateDue, type).add(EntryFactory.createEntry(type, description, dateDue, startTime, endTime));
-		else
-			System.out.println("Not valid Entry");
-	}
-	
-	@Override
-	public boolean isValidEntry(Date date, TimeRange timeRange) {
-		// TODO Auto-generated method stub
-		if(this.getEntries(date).contains(timeRange))
-			return true;
-		return false;
+		
+		String strYear = dateDue.toString().substring(dateDue.toString().length()-4, dateDue.toString().length());
+		int year = Integer.parseInt(strYear);		
+		GregorianCalendar calendar = new GregorianCalendar(year, dateDue.getMonth(), dateDue.getDate());		
+		int month = calendar.get(java.util.Calendar.MONTH);
+		int day = calendar.get(java.util.Calendar.DATE);	
+		
+		int minYear = java.util.Calendar.getInstance().get(java.util.Calendar.YEAR)-100;
+		int indexYear = year-minYear;		
+		int indexMonth = month;
+		int indexDay = day-1;
+		
+		int startIndex = Application.getTimeIndex(startTime);
+		int endIndex = Application.getTimeIndex(endTime);
+		if(type == EntryType.TASK)
+			endIndex = startIndex+1;
+		
+		for(int i = startIndex; i < endIndex; i++) {
+			this.calendar.getYears().get(indexYear).getMonths().get(indexMonth).getDays().get(indexDay).getEntries().set(i, 
+					EntryFactory.createEntry(type, description, dateDue, startTime, endTime));
+		}
+		
+//		this.calendar.getYears().get(indexYear).getMonths().get(indexMonth).getDays().get(indexDay).getEntries().set(Application.getTimeIndex(startTime), 
+//				EntryFactory.createEntry(type, description, dateDue, startTime, endTime));
+		System.out.println("MODEL added to date: "+Application.getTimeIndex(startTime));
 	}
 
 	@Override
-	public void removeEntries(Entry entry) {
-		// TODO Auto-generated method stub
-		this.getEntries(entry.getDateDue()).remove(entry);
+	public boolean isValidEntry(Date date, TimeRange timeRange) {
+		ArrayList<Entry> entries = this.getEntries(date);
+
+		int startIndex = Application.getTimeIndex(timeRange.getStartTime());
+		int endIndex = Application.getTimeIndex(timeRange.getEndTime());
+		
+		if(startIndex >= endIndex)
+			return false;
+		
+		for(int i = startIndex; i < endIndex; i++) {
+			if(entries.get(i) != null) {
+				return false;
+			}
+		}
+		return true;
 	}
 
 	@Override
@@ -101,7 +135,22 @@ public class ProductivityToolModel extends Model {
 		ExportCsv export = new ExportCsv();
 		export.generate("entries", entries);
 	}
-	
+
+	@Override
+	public void removeEntries(Entry entry) {
+		System.out.println("remove");
+		if(entry != null) {
+			ArrayList<Entry> entries = this.getEntries(entry.getDateDue());
+			int startIndex = Application.getTimeIndex(entry.getTimeRange().getStartTime());
+			int endIndex = Application.getTimeIndex(entry.getTimeRange().getEndTime());
+			
+			for(int i = startIndex; i < endIndex; i++) {
+				entries.set(i, null);		
+				System.out.println("Removed index: "+i);
+			}	
+		}
+	}
+
 	@Override
 	public void importFile(JFrame baseFrame) {
 		ArrayList<Entry> importedEntries = new ArrayList<Entry>();
@@ -124,6 +173,5 @@ public class ProductivityToolModel extends Model {
 			this.getEntries(importedEntries.get(i).getDateDue()).add(importedEntries.get(i));
 		}
 	}
-
 	
 }
