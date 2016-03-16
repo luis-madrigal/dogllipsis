f92afd4372faca13869364d5e0fd4819efb345f7
MZRaymundo <mz.raym@gmail.com>
2016-03-16 20:29:47 +0900

    Update AgendaView.java

--- [02] DesignChallenge/src/com/view/AgendaView.java
+++ [02] DesignChallenge/src/com/view/AgendaView.java
@@ -4,26 +4,22 @@ public class AgendaView extends EntryView{
 
 	@Override
 	public void viewTasks() {
-		// TODO Auto-generated method stub
-		
+	
 	}
 
 	@Override
 	public void viewEvents() {
-		// TODO Auto-generated method stub
 		
 	}
 
 	@Override
 	public void viewAll() {
-		// TODO Auto-generated method stub
-		
+	
 	}
 
 	@Override
 	public void viewNone() {
-		// TODO Auto-generated method stub
-		
+	
 	}
 
 }
